873d5c2113af57e1e18020aecf78ab38a6f67ee5
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-05 11:32:09 +0600

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/com/example/springpracticesecondrestclient/SpringPracticeSecondRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticesecondrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeSecondRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeSecondRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticesecondrestclient/SpringPracticeSecondRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticesecondrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeSecondRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
